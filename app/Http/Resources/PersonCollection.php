4c0b066a5d5acd9f8f8a5dd4b9e3f36c6eac4968
uni51 <uni51@users.noreply.github.com>
2019-09-01 16:40:00 +0900

    Modify：Update PersonResource(Return BMI)  Clear:Test

--- app/Http/Resources/PersonCollection.php
+++ app/Http/Resources/PersonCollection.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Services\BmiService;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PersonCollection extends ResourceCollection
@@ -15,14 +14,6 @@ class PersonCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'height' => $this->height,
-            'weight' => $this->weight,
-            'BMI' => BmiService::getBmi($this->resource),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ];
+        return parent::toArray($request);
     }
 }
