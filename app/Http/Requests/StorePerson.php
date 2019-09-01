ec6a7afd82af3e4f68d92c6790b3d92f204f8e5f
uni51 <uni51@users.noreply.github.com>
2019-09-01 16:44:52 +0900

    Define validation Rules for Person

--- app/Http/Requests/StorePerson.php
+++ app/Http/Requests/StorePerson.php
@@ -13,7 +13,7 @@ class StorePerson extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class StorePerson extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'height' => 'required|numeric|min:0.1',
+            'weight' => 'required|numeric|min:0.1',
         ];
     }
 }
--- app/Http/Requests/UpdatePerson.php
+++ app/Http/Requests/UpdatePerson.php
@@ -13,7 +13,7 @@ class UpdatePerson extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,9 @@ class UpdatePerson extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'max:255',
+            'height' => 'numeric|min:0.1',
+            'weight' => 'numeric|min:0.1',
         ];
     }
 }
